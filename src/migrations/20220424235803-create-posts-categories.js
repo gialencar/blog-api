aa82cff84fe3de7ffc9755190f2c6fd4c162c232
Gilson Alencar <gilson.alencar94@gmail.com>
2022-04-27 17:53:51 -0300

    feat: create migrations

---
+++ src/migrations/20220424235803-create-posts-categories.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostsCategories', {
+      postId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'BlogPosts',
+          key: 'id',
+        },
+        primaryKey: true,
+        onDelete: 'CASCADE',
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Categories',
+          key: 'id',
+        },
+        primaryKey: true,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostsCategories');
+  },
+};

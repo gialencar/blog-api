a7ad1bd7838437db742d0d2604506fe094f0e1d3
Gilson Alencar <gilson.alencar94@gmail.com>
2022-04-30 17:39:42 -0300

    feat: create login controller

---
+++ src/controllers/login.controller.js
@@ -0,0 +1,13 @@
+const { loginService } = require('../services');
+
+async function loginController(req, res) {
+  const { email, password } = req.body;
+
+  const token = await loginService.generateAccessToken({ email, password });
+
+  if (!token) return res.status(400).json({ message: 'Invalid fields' });
+
+  res.status(200).json({ token });
+}
+
+module.exports = loginController;

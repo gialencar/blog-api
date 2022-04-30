825a818358b52ea1c89c78556642ceecf190ce2d
Gilson Alencar <gilson.alencar94@gmail.com>
2022-04-30 17:42:11 -0300

    feat: configure login route

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const errorHandler = require('./src/middlewares/errorHandler');
+const loginRouter = require('./src/routes/login.routes');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
@@ -8,5 +10,9 @@ app.use(express.json());
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/login', loginRouter);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));

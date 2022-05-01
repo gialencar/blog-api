db1cee4d7042bec3de995b1445b02ca2c9476e3d
Gilson Alencar <gilson.alencar94@gmail.com>
2022-04-30 23:29:08 -0300

    feat(routes): add route POST:/user/ for registering new users

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,9 @@
 const userRouter = require('express').Router();
 const userController = require('../controllers/user.controller');
+const validateUser = require('../middlewares/validateUser');
 
 userRouter.get('/', userController.index);
+
+userRouter.post('/', validateUser, userController.register);
 
 module.exports = userRouter;

3d3eb4883b6cafe5285f3a8a37b136ef80295941
Gilson Alencar <gilson.alencar94@gmail.com>
2022-04-30 23:25:06 -0300

    feat(service/user): add function to register a new user

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,4 +1,5 @@
 const { User } = require('../models');
+const { generateToken } = require('./login.service');
 
 async function index() {
   const users = await User.findAll({
@@ -8,6 +9,18 @@ async function index() {
   return users;
 }
 
+async function register({ displayName, email, password, image }) {
+  const userExists = await User.findOne({ where: { email } });
+  if (userExists) return;
+
+  await User.create({ displayName, email, password, image });
+
+  const token = generateToken({ email });
+
+  return token;
+}
+
 module.exports = {
   index,
+  register,
 };

75129a742f3fe926eb53fc951fffc725d552e338
Gilson Alencar <gilson.alencar94@gmail.com>
2022-04-30 23:27:49 -0300

    feat:(controller): add register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -23,6 +23,19 @@ async function index(req, res) {
   res.status(200).json(users);
 }
 
+async function register(req, res) {
+  const { displayName, email, password, image } = req.body;
+
+  const token = await userService.register({ displayName, email, password, image });
+
+  if (!token) {
+    return res.status(409).json({ message: 'User already registered' });
+  }
+
+  res.status(201).json({ token });
+}
+
 module.exports = {
   index,
+  register,
 };
